0637335acbc86d075a02d042d448fe338c54784b
Ray Boyd <hi@rayboyd.co.uk>
2025-04-20 10:44:16 +0100

    feat(config): better naming for validator, it does not need to be public

--- internal/app/validate.go
+++ internal/app/validate.go
@@ -2,19 +2,19 @@ package app
 
 import "github.com/go-playground/validator/v10"
 
-type AppValidator struct {
-	validate *validator.Validate
+type aDataValidator struct {
+	validator *validator.Validate
 }
 
-var av AppValidator
+var av aDataValidator
 
 func init() {
-	av.validate = validator.New()
+	av.validator = validator.New()
 
 	// Register custom validation functions here.
 	// See: https://pkg.go.dev/github.com/go-playground/validator/v10#hdr-Custom_Validation_Functions
 }
 
 func GetValidator() *validator.Validate {
-	return av.validate
+	return av.validator
 }
